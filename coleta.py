1488858d6ec67b4b6b1464b582366d36289ef9f3
Igor Lima <limasigor2@gmail.com>
2018-10-27 16:38:41 -0300

    Pegando os tweets da #FLAxPAL e salvando na base: id do tweet, texto, usuário, retweeted_status, retweeted_status

--- coleta.py
+++ coleta.py
@@ -2,11 +2,10 @@
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
-
 import json
 
 import keys
-from util import stopwords_files_to_list
+import util 
 # Go to http://dev.twitter.com and create an app.
 # The consumer key and secret will be generated for you after
 consumer_key=keys.CONSUMER_KEY
@@ -17,49 +16,45 @@ consumer_secret=keys.CONSUMER_SECRET
 access_token=keys.ACCESS_TOKEN
 access_token_secret=keys.ACCESS_TOKEN_SECRET
 
-# create a list of stopwords in English, Spanish and Portuguese.
-stop_words = stopwords_files_to_list()
 class StdOutListener(StreamListener):
-   """ A listener handles tweets are the received from the stream.
-   This is a basic listener that just prints received tweets to stdout.
+    """
+    A listener handles tweets are the received from the stream.
+    This is a basic listener that just prints received tweets to stdout.
+    """
+    def on_data(self, data):
+        #print (data)
+        json_data = json.loads(data)
+        id = ""
+        user = ""
+        retweeted_status = False
+        originalUser = ""
+        text = ""
+        if 'id' in json_data:
+            id = str(json_data['id'])
+        if 'user' in json_data:
+            user = ascii(json_data['user']['name'])
+        if 'retweeted_status' in json_data:
+            retweeted_status = True
+            originalUser = ascii(json_data['retweeted_status']['user']['name'])
+        if 'text' in json_data:
+            text = util.pre_process_tweet(json_data['text'])
+        util.persist_at(conn, id, text, user, retweeted_status , originalUser)
+       
+        return True
 
-   """
-   def on_data(self, data):
-       #print (data)
-       json_data = json.loads(data)
-       text = ""
-       palavra = ""
-       if 'user' in json_data:
-           text = ascii(json_data['text'])
-           for elem in text.split():
-              if palavra not in stop_words:
-                 palavra = elem
-       if (palavra != ""):
-           print("\"" + palavra + "\"" + "\n")
-           file.write("\"" + palavra + "\"" + "\n")
-
-       # user = "---"
-       # originalUser = "---"
-       # if 'user' in json_data:
-       #     user = ascii(json_data['user']['name'])
-       # if 'retweeted_status' in json_data:
-       #     originalUser = ascii(json_data['retweeted_status']['user']['name'])
-       # if (user != "---"):
-       #     print("\"" + user + "\" \"" + originalUser +"\"" + "\n")
-       #     file.write("\"" + user + "\" \"" + originalUser +"\"" + "\n")
-       return True
-
-   def on_error(self, status):
-       print (status)
-       # if status_code == 420:
-       #     return True
+    def on_error(self, status):
+        print (status)
+        if status == 420:
+            return True
 
 if __name__ == '__main__':
-   file = open('teste.txt', 'a')
-   l = StdOutListener()
-   auth = OAuthHandler(consumer_key, consumer_secret)
-   auth.set_access_token(access_token, access_token_secret)
+    l = StdOutListener()
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
 
-   stream = Stream(auth, l)
-   setTerms = ['Boca', 'Benedetto', '#CagueiproIbope ']
-   stream.filter(track=setTerms)
+    conn = util.conexao()
+
+    stream = Stream(auth, l)
+    setTerms = ['#FLAxPAL']
+    stream.filter(track=setTerms)
+    conn.close()
--- util.py
+++ util.py
@@ -1,5 +1,7 @@
 import os
-import time
+import re
+import sqlite3
+import keys
 
 def stopwords_files_to_list():
 	lista = []
@@ -8,3 +10,32 @@ def stopwords_files_to_list():
 			lista.append((file.read().splitlines()))
 	return lista
 
+def remove_stop_words(tweet):
+    new_words = ''
+    for word in tweet.split(' '):
+        if word not in stopwords_files_to_list():
+            new_words += ' ' + word 
+    return new_words
+
+def remove_link(tweet):
+	return re.sub(r"http\S+", "", tweet)
+
+def remove_metions(tweet):
+	new_tweet = ''
+	for word in tweet.split(" "):
+		if '@' not in word:
+			new_tweet += " " + word
+	return new_tweet
+
+def pre_process_tweet(tweet):
+	return remove_link(remove_stop_words(remove_metions(tweet)))
+
+def conexao():
+    return sqlite3.connect(keys.DB_PATH)
+
+def persist_at(conn, id, text, user, retweet_status, original_user):
+	if original_user == "":
+		original_user = "' '"
+	conn.cursor().execute("INSERT INTO tweets VALUES ('{}', '{}', {}, {}, {})".format(id, text, user, retweet_status, original_user))
+
+	conn.commit()
